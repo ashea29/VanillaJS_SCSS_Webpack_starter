97c05e0e088c0d76b21da6f2d5fc0a599bd8cfa0
WebHelix <andrew.shea@webhelix.dev>
2022-08-25 20:43:46 +0000

    Added starting webpack configs for dev and prod

---
+++ webpack.production.config.js
@@ -0,0 +1,91 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: {
+    home: "./src/pages/home.module.js",
+  },
+  output: {
+    filename: "[name].[contenthash].js",
+    path: path.resolve(__dirname, "./dist"),
+    publicPath: "",
+  },
+  mode: "production",
+  resolve: {
+    alias: {
+      "@components": path.resolve(__dirname, "./src/components/"),
+      "@pages": path.resolve(__dirname, "src/pages/"),
+      "@utils": path.resolve(__dirname, "src/utils/"),
+    },
+  },
+  optimization: {
+    splitChunks: {
+      chunks: "all",
+      minSize: 3000,
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpg)$/,
+        type: "asset",
+        parser: {
+          dataUrlCondition: {
+            maxSize: 3 * 1024,
+          },
+        },
+      },
+      {
+        test: /\.txt/,
+        type: "asset/source",
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/env"],
+            plugins: ["@babel/plugin-proposal-class-properties"],
+          },
+        },
+      },
+      {
+        test: /\.hbs$/,
+        use: ["handlebars-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash].css",
+    }),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      chunks: ["home"],
+      title: "Home",
+      description: "Home page",
+      template: "src/page-template.hbs",
+      minify: false,
+    }),
+    // new HtmlWebpackPlugin({
+    //     filename: 'kiwi.html',
+    //     chunks: ['kiwi'],
+    //     title: 'Kiwi',
+    //     description: 'Kiwi',
+    //     template: 'src/page-template.hbs',
+    //     minify: false
+    // })
+  ],
+};
